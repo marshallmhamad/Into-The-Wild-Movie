2516890585e2ba5ab203be14cc916f1c35c4b96c
marshall <113690263+marshallmhamad@users.noreply.github.com>
2023-02-11 11:29:44 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const container = document.querySelector("ul");
+container.classList.add("nav_links");
+
+const Cast = document.querySelector("section");
+Cast.classList.add("Cast");
+
+const footer = document.querySelector("footer");
+footer.classList.add("zone");
